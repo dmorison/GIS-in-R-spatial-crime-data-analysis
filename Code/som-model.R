17b4062cc3620cfd6d666d2bc0de201fc092eba9
David Morison <davemorison@gmail.com>
2017-03-21 08:27:07 +0000

    mapping the data points to nodes and inspecting values

--- Code/som-model.R
+++ Code/som-model.R
@@ -8,7 +8,7 @@ crimeTypes <- read.csv("Data/crime-types.csv")
 allvars <- ncol(crimeTypes)
 varFrom <- 2
 varTo <- allvars
-vars <- c(2, 3, 6, 14)
+vars <- c(3, 5, 14)
 dataTrain <- crimeTypes[, vars]
 dataTrainMatrix <- as.matrix(scale(dataTrain))
 names(dataTrainMatrix) <- names(dataTrain)
@@ -17,7 +17,7 @@ somGrid <- somgrid(xdim = 20, ydim = 20, topo = "hexagonal")
 
 system.time(somModel <- som(dataTrainMatrix,
                 grid = somGrid,
-                rlen = 400,
+                rlen = 200,
                 alpha = c(0.05,0.01),
                 keep.data = TRUE))
 
@@ -43,6 +43,49 @@ for(i in 1:length(vars)) {
        main = pTitle, palette.name = coolBlueHotRed)
   dev.off()
 }
+
+var <- 3
+plot(somModel, type = "property", property = getCodes(somModel, 1)[, var],
+     main = colnames(getCodes(somModel, 1))[var], palette.name = coolBlueHotRed)
+plot(somModel, type = "codes")
+
+# get values of first 6 and last 6 nodes
+getCodes(somModel, 1)[c(1:6, 395:400), ]
+# get the nodes the first 6 data points are mapped to
+somModel$unit.classif[1:6]
+# get the values of these nodes
+getCodes(somModel, 1)[c(399, 358, 255, 151, 295, 400), ]
+# compare the data points values with the node values it was mapped to
+# should be the same as previous step
+dataTrainMatrix[1:6, ]
+# match the nodes back to the original data points before scaling
+dataTrain[1:6, ]
+result <- data.frame(cbind(crimeTypes[, 1], dataTrain, somModel$unit.classif))
+result[1:6, ]
+
+# inspection
+summary(result)
+result[result$criminal_damage_and_arson == 168, ]
+getCodes(somModel, 1)[380, ]
+result[result$bicycle_theft == 216, ]
+getCodes(somModel, 1)[400, ]
+result[result$vehicle_crime == 179, ]
+getCodes(somModel, 1)[360, ]
+
+######
+attr(dataTrainMatrix, 'scaled:scale')
+######
+# install.packages("stats")
+# library(stats)
+# original <- unscale(dataTrainMatrix, dataTrainMatrix)
+
+# varUnscaled <- aggregate(as.numeric(dataTrain[, var]), by = list(somModel$unit.classif),
+#                           FUN = mean, simplify = TRUE)
+plot(somModel, type = "property", property = varUnscaled[, 2], main = colnames(getCodes(somModel, 1))[var],
+     palette.name = coolBlueHotRed)
+
+
+
 
 
 
